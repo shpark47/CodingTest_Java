e288706453804152c05b36559848ac47e62a7249
shpark <shp04078@naver.com>
2024-12-19 16:43:26 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 35.56 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,13 +3,13 @@ class Solution {
         String answer = "";
         int[] level = new int[food.length];
         
-        level[0] = 0;
+        level[0] = 0; // 0레벨(물)은 무조건 1개라 따로 넣어줌
         
-        for(int i = 1; i < food.length; i++){
+        for(int i = 1; i < food.length; i++){ // 레벨마다 2로나눈 개수를 넣음 -> 한사람 몫의 음식개수
             level[i] = food[i]/2;
         }
         
-        for(int i = 0; i < level.length; i++){
+        for(int i = 0; i < level.length; i++){ // 음식 개수의 레벨을 넣음
             for(int j = 0; j < level[i]; j++){
                 answer += i;
             }
@@ -17,7 +17,7 @@ class Solution {
         
         answer += 0;
         
-        for(int i = level.length-1; i > 0; i--){
+        for(int i = level.length-1; i > 0; i--){ // 반대로 넣음
             for(int j = level[i]; j > 0; j--){
                 answer += i;
             }
