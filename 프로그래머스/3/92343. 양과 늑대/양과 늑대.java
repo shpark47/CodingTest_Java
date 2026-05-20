3084ffca536eca2206cc09df6c03787558d4b53d
seonghun03 <shp04078@naver.com>
2026-05-20 10:14:39 +0900

    [level 3] Title: 양과 늑대, Time: 1.00 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,35 +1,29 @@
 class Solution {
-    boolean[] visited;
     int max = 0;
+    boolean[] visited;
     public int solution(int[] info, int[][] edges) {
         visited = new boolean[info.length];
         visited[0] = true;
-        
         dfs(info, edges, 1, 0);
-        
         return max;
     }
     
     void dfs(int[] info, int[][] edges, int sheep, int wolf) {
-        if(wolf >= sheep) {
-            return;
-        }
+        if(wolf >= sheep) return;
         
         max = Math.max(max, sheep);
         
-        for(int[] edge : edges) {
-            int parent = edge[0];
-            int child = edge[1];
-            if(visited[parent] && !visited[child]) {
-                visited[child] = true;
-                
-                if(info[child] == 0) {
+        for(int[] e : edges) {
+            int p = e[0];
+            int c = e[1];
+            if(visited[p] && !visited[c]) {
+                visited[c] = true;
+                if(info[c] == 0) {
                     dfs(info, edges, sheep + 1, wolf);
                 } else {
                     dfs(info, edges, sheep, wolf + 1);
                 }
-                
-                visited[child] = false;
+                visited[c] = false;
             }
         }
     }
